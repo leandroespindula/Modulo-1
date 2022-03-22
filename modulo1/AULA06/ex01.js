5bda6553ce91ccfcb8276ef94a034108e93b75fb
Leandro Espindula <leandroepindula@gmail.com>
2022-03-21 22:13:18 -0300

    Ultimas atualizações

--- modulo1/AULA06/ex01.js
+++ modulo1/AULA06/ex01.js
@@ -1,4 +1,4 @@
-Faça um Programa que leia 20 números inteiros e armazene-os num array. Armazene os números pares no array PAR e os números IMPARES no array IMPAR. Imprima os três vetores.
-Faça um Programa que peça a idade e a altura de 5 pessoas, armazene cada informação no seu respectivo array. Imprima a média de idade e de altura.
-Faça um programa em que o usuário escolha a quantidade de alunos a serem cadastrados e peça o nome e nota de cada um. Armazene as informações em duas listas separadas e no final imprima o nome e a nota correspondente de cada aluno, e a média total da notas.
-Faça um programa que simule um lançamento de dados. Lance o dado 100 vezes e armazene os resultados em um array. Depois, mostre quantas vezes cada valor foi conseguido
+// Faça um Programa que leia 20 números inteiros e armazene-os num array. Armazene os números pares no array PAR e os números IMPARES no array IMPAR. Imprima os três vetores.
+// Faça um Programa que peça a idade e a altura de 5 pessoas, armazene cada informação no seu respectivo array. Imprima a média de idade e de altura.
+// Faça um programa em que o usuário escolha a quantidade de alunos a serem cadastrados e peça o nome e nota de cada um. Armazene as informações em duas listas separadas e no final imprima o nome e a nota correspondente de cada aluno, e a média total da notas.
+// Faça um programa que simule um lançamento de dados. Lance o dado 100 vezes e armazene os resultados em um array. Depois, mostre quantas vezes cada valor foi conseguido
--- modulo1/AULA07/CodeLab.js
+++ modulo1/AULA07/CodeLab.js
@@ -1,7 +1,7 @@
+console.clear;
 prompt = require ('prompt-sync')();
 
 let rodadas; //variaveis auxiliares
-let rodadasAuxiliar; //variaveis auxiliares
 
 let continuar = 1; // variavel para iniciar o jogo
 
@@ -18,62 +18,72 @@ while (continuar == 1) {
 
   // Pergunta quantidade de rodadas
   rodadas = +prompt('Quantas rodadas deseja jogar: ');
-  rodadasAuxiliar = rodadas;
-  rodadas = 0;
   
-  while (rodadas < rodadasAuxiliar) {
+  for (let i = 0; i < rodadas ; i++) {
+    console.clear;
+    console.log(`${i + 1} de ${rodadas}\n\n`);
     jogador = +prompt('Escolha Pedra(0), Papel(1) e Tesoura(2): ');
-    maquina = Math.floor(Math.random()*3);
+    maquina = Math.floor(Math.random()*opcoes.length);
 
-    //Mostrar o vencedor e numero de vitorias da maquina e jogador humano.
+    //Soma as vitorias do jogador humano e da maquina.
 
-  if(jogador == 0){
-    if(maquina == 1){
-      numeroVitoriaMaquina++;
-      
-    }else if(maquina == 2){
-      numeroVitoriaHumano++;
-      
-    }
-    
-  }
-    
-  else if(jogador == 1){
-    if(maquina == 0){
-      numeroVitoriaMaquina++;
-      
-    }else if(maquina == 2){
-      numeroVitoriaHumano++;
+    if(jogador == 0){
+      if(maquina == 1){
+        numeroVitoriaMaquina++;
+        
+      }else if(maquina == 2){
+        numeroVitoriaHumano++;
+        
+      }
       
     }
-    
-  }
-    
-  else if(jogador == 2){
-    if(maquina == 1){
-      numeroVitoriaMaquina++;
       
-    }else if(maquina == 0){
-      numeroVitoriaHumano++;
+    else if(jogador == 1){
+      if(maquina == 0){
+        numeroVitoriaMaquina++;
+        
+      }else if(maquina == 2){
+        numeroVitoriaHumano++;
+        
+      }
       
     }
+      
+    else if(jogador == 2){
+      if(maquina == 1){
+        numeroVitoriaMaquina++;
+        
+      }else if(maquina == 0){
+        numeroVitoriaHumano++;
+        
+      }
+
+
     
-  }
-    rodadas++;
+    }
+
     
-  }
+     //Mostra as mensagens de quem venceu e os placares!
+
+    
   
+  }
+
   if(numeroVitoriaHumano > numeroVitoriaMaquina){
     console.log("Você venceu! ");
-    console.log(`${numeroVitoriaHumano} x ${numeroVitoriaMaquina}`);
+    console.log(`HUMANO ${numeroVitoriaHumano} x ${numeroVitoriaMaquina} MAQUINA\n\n`);
   }else if (numeroVitoriaMaquina > numeroVitoriaHumano){
     console.log("A maquina venceu!");
-    console.log(`${numeroVitoriaHumano} x ${numeroVitoriaMaquina}`);
+    console.log(`HUMANO ${numeroVitoriaHumano} x ${numeroVitoriaMaquina} MAQUINA\n\n`);
   }else{
-    console.log(`${numeroVitoriaHumano} x ${numeroVitoriaMaquina}`);
+    console.log("Empate");
+    console.log(`HUMANO ${numeroVitoriaHumano} x ${numeroVitoriaMaquina} MAQUINA\n\n`);
   }
 
   continuar = +prompt('Deseja reiniciar o jogo: sim(1) nao(0)');
+
 }
+
+
 
 console.log('Jogo Encerrado!');
